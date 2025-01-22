872142d0fab873209df48d08a6544958f07f8557
LLorintz <lorintz.levente@mechwart.com>
2025-01-22 08:33:42 +0100

    Update login.tsx

--- login.tsx
+++ login.tsx
@@ -2,11 +2,14 @@
 import React, { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
 
   const [username, setUsername] = useState<string>('')
   const [password, setPassword] = useState<string>('')
   
+
   const handleusername = (e:React.ChangeEvent<HTMLInputElement>)=>{
     setUsername(e.target.value);
   }
@@ -24,6 +27,21 @@ function App() {
   })
   const data = await response.json()
   console.log(data)
+  localStorage.setItem('JWT',data.token)
+ }
+
+ const handleGetinfo=async()=>{
+  const token = localStorage.getItem('JWT')
+  const response = await fetch(`http://localhost:3000/accounts/${username}`,{
+    method:'Get',
+    headers:{
+      'Authorization':`Bearer ${token}`  
+    }
+    
+  })
+  const data = await response.json()
+  console.log(data)
+
 
  }
 
@@ -32,6 +50,7 @@ function App() {
       <input value={username} onChange={handleusername} type="text" placeholder='username'/>
       <input value={password} onChange={handlepassword} type="password" placeholder='password'/>
       <button onClick={handleLogin}>login</button>
+      <button onClick={handleGetinfo}>Get info</button>
     </div>
   )
 }
